deb1b53b372e6dc2b26c0dd318087d2c0b123bb8
Shubham Dokania <skd.1810@gmail.com>
2015-08-07 22:59:56 +0530

    Making changes to TIC TAC TOE Board

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -3,10 +3,16 @@ class Board():
     def __init__(self):
         self.board = [['-' for j in range(3)] for i in range(3)]
 
-    def printBoard(self):
+    def print_board(self):
         print self.board[0]
         print self.board[1]
         print self.board[2]
+
+    def check_winner(self):
+
+        # method to check if a player has won and return the player number who has won.
+
+        return 0
 
 
 if __name__ == '__main__':
